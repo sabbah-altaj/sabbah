6815edd9e0f7d41d3567819d226821eaa0385c4c
Byte Master <147357721+bytemmaster@users.noreply.github.com>
2023-10-18 22:05:55 +0200

    Add files via upload

---
+++ frontend/components/header.php
@@ -0,0 +1,31 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+<header class="p-2">
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-between">
+            <div class="logo">
+                <!-- <h4><a href="" class="nav-link">sabbah<span class="logo-ex text-uppercase">.net</span></a></h4> -->
+                <h4 class="m-0"><a href="" class="nav-link"><img src="../images/me2.jpg" alt=""></a></h4>
+                <!-- <h4 class="m-0"><a href="" class="nav-link">sabbah</a></h4> -->
+            </div>
+
+            <div class="links d-none d-md-block">
+                <ul class="list-unstyled m-0 d-flex align-items-center">
+                    <li class="me-3"><a href="" class="nav-link">Contact</a></li>
+                    <li class="me-3"><a href="" class="nav-link">Features</a></li>
+                    <li class="me-3"><a href="" class="nav-link">Portfolio</a></li>
+                    <li class="me-3"><a href="" class="nav-link">Services</a></li>
+                    <li class="me-3"><a href="" class="nav-link">Skills</a></li>
+                    <li class="me-3"><a href="ar/index.php" class="nav-link">🇸🇩 Arabic</a></li>
+                </ul>
+            </div>
+
+            <div class="humburger-menu d-block d-md-none">
+                <i class="fas fa-bars"></i>
+            </div>
+
+        </div>
+    </div>
+</header>
